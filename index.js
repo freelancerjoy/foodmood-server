7722470068b479f1720ad5410f71862b440d4d78
Md Zihad Shah <freelancerjoy10@gmail.com>
2023-05-02 20:31:13 +0600

    Create data folder and create chefs json file and install cors for share data

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const port = process.env.PORT || 5000;
+app.use(cors());
+const chefs = require("./Data/chefs.json");
 
 app.get("/", (req, res) => {
   res.send("Food Mood");
+});
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
