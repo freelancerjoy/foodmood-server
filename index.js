3848db02525840d90a876b86490ba12aa9cb82bf
Md Zihad Shah <freelancerjoy10@gmail.com>
2023-05-03 07:20:09 +0600

    recipe data cretaed and upodate with chefs id

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
+
 const chefs = require("./Data/chefs.json");
+const recipe = require("./Data/recipe.json");
 
 app.get("/", (req, res) => {
   res.send("Food Mood");
 });
+
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  const chefRecipes = recipe.filter((recipe) => id == recipe.recipe_id);
+  res.send(chefRecipes);
 });
 
 app.listen(port, () => {
